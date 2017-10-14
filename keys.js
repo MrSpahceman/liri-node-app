10cb677723f6d21115fb148999a6b7ae9ea63849
MrSpahceman <palacc25@gmail.com>
2017-10-13 18:23:25 -0700

    new

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: 'AtQ8LgDZXw4h24GR1fGMunoVN',
+    consumer_secret: '	QMPXcHK2TrWNa4BFj6RRasrn0Mfe02TvsM62u0xnF7aaIqg5Z9',
+    access_token_key: '915265467240955904-hH8GsBi5E6xCVnEIyfGYq1nJe7d2g0G',
+    access_token_secret: '	l6BZg5N2mF2ttkSPjmzInbE632tNuc1ltPjSJuLyt80KV',
+}
+
+module.exports = twitterKeys;
